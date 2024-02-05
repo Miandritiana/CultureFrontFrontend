d34b7ee903e39a26276e6f10cb5d365755e672b3
Miandritiana <miandrititana21@gmail.com>
2024-02-05 16:55:32 +0300

    naverina mitsy lou fa tsy mety

--- src/views/message/components/Chat.js
+++ src/views/message/components/Chat.js
@@ -1,124 +1,127 @@
-import React, { useEffect } from 'react';
-// import $ from 'jquery';
-// import 'jquery-mousewheel';
+import React, { useEffect, useState, useRef } from 'react';
 import './Chat.css';
 
 const Chat = () => {
+    const [messages, setMessages] = useState([]);
+    const messageInputRef = useRef(null);
 
-    // let d, h, m;
-    // let i = 0;
+    let d, m;
+    let i = 0;
 
-    // useEffect(() => {
-    //     const $messages = $('.messages-content');
-    //     $messages.mCustomScrollbar();
+    useEffect(() => {
+        // Initialize messages or load from the backend
+        fakeMessage();
 
-    //     setTimeout(() => {
-    //     fakeMessage();
-    //     }, 100);
+        // Focus on the message input when the component mounts
+        messageInputRef.current.focus();
+    }, []);
 
-    //     return () => {
-    //     // Clean up if needed
-    //     };
-    // }, []);
+    const updateScrollbar = () => {
+        // If you want to scroll to the bottom of the messages, you can use the ref
+        // For example, if you set a ref for the messages container:
+        // messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+    };
 
-    // const updateScrollbar = () => {
-    //     $('.messages-content').mCustomScrollbar('update').mCustomScrollbar('scrollTo', 'bottom', {
-    //     scrollInertia: 10,
-    //     timeout: 0
-    //     });
-    // };
+    const setDate = () => {
+        d = new Date();
+        if (m !== d.getMinutes()) {
+            m = d.getMinutes();
+            setMessages(prevMessages => [
+                ...prevMessages,
+                { type: 'timestamp', text: `${d.getHours()}:${m}` }
+            ]);
+        }
+    };
 
-    // const setDate = () => {
-    //     d = new Date();
-    //     if (m !== d.getMinutes()) {
-    //     m = d.getMinutes();
-    //     $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
-    //     }
-    // };
+    const insertMessage = () => {
+        const msg = messageInputRef.current.value.trim();
+        if (!msg) return;
 
-    // const insertMessage = () => {
-    //     const msg = $('.message-input').val();
-    //     if ($.trim(msg) === '') {
-    //     return false;
-    //     }
+        setMessages(prevMessages => [
+            ...prevMessages,
+            { type: 'personal', text: msg }
+        ]);
 
-    //     $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-    //     setDate();
-    //     $('.message-input').val(null);
-    //     updateScrollbar();
+        setDate();
 
-    //     setTimeout(() => {
-    //     fakeMessage();
-    //     }, 1000 + (Math.random() * 20) * 100);
-    // };
+        // Clear the input field
+        messageInputRef.current.value = '';
 
-    // $('.message-submit').click(() => {
-    //     insertMessage();
-    // });
+        updateScrollbar();
 
-    // $(window).on('keydown', (e) => {
-    //     if (e.which === 13) {
-    //     insertMessage();
-    //     return false;
-    //     }
-    // });
+        setTimeout(() => {
+            fakeMessage();
+        }, 1000 + Math.random() * 20 * 100);
+    };
 
-    // const Fake = [
-    //     'Hi there, I\'m Fabio and you?',
-    //     'Nice to meet you',
-    //     'How are you?',
-    //     'Not too bad, thanks',
-    //     'What do you do?',
-    //     'That\'s awesome',
-    //     'Codepen is a nice place to stay',
-    //     'I think you\'re a nice person',
-    //     'Why do you think that?',
-    //     'Can you explain?',
-    //     'Anyway I\'ve gotta go now',
-    //     'It was a pleasure chat with you',
-    //     'Time to make a new codepen',
-    //     'Bye',
-    //     ':)'
-    // ];
+    const handleKeyDown = (e) => {
+        if (e.which === 13) {
+            insertMessage();
+        }
+    };
 
-    // const fakeMessage = () => {
-    //     if ($('.message-input').val() !== '') {
-    //     return false;
-    //     }
+    const Fake = [
+        'Hi there, I\'m Fabio and you?',
+        'Nice to meet you',
+        'How are you?',
+        // ... other messages
+    ];
 
-    //     $('<div class="message loading new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure><span></span></div>').appendTo($('.mCSB_container'));
-    //     updateScrollbar();
+    const fakeMessage = () => {
+        if (messageInputRef.current.value.trim() !== '') {
+            return;
+        }
 
-    //     setTimeout(() => {
-    //     $('.message.loading').remove();
-    //     $('<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
-    //     setDate();
-    //     updateScrollbar();
-    //     i++;
-    //     }, 1000 + (Math.random() * 20) * 100);
-    // };
+        setMessages(prevMessages => [
+            ...prevMessages,
+            { type: 'loading' }
+        ]);
 
+        updateScrollbar();
+
+        setTimeout(() => {
+            setMessages(prevMessages => [
+                ...prevMessages.slice(0, -1),
+                { type: 'new', text: Fake[i] }
+            ]);
+            setDate();
+            updateScrollbar();
+            i++;
+        }, 1000 + Math.random() * 20 * 100);
+    };
 
     return (
-        <div class="chat">
-            <div class="chat-title">
-                <h1>Fabio Ottaviani</h1>
-                <h2>Supah</h2>
-                <figure class="avatar">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>
+        <div className="chat">
+            {/* ... other components */}
+            <div className="messages">
+                <div className="messages-content">
+                    {/* Render messages */}
+                    {messages.map((message, index) => (
+                        <div key={index} className={`message ${message.type}`}>
+                            {message.type === 'loading' ? (
+                                <figure className="avatar">
+                                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" alt="loading" />
+                                </figure>
+                            ) : null}
+                            <span>{message.text}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div class="messages">
-                <div class="messages-content"></div>
+            <div className="message-box">
+                <textarea
+                    ref={messageInputRef}
+                    type="text"
+                    className="message-input"
+                    placeholder="Type message..."
+                    onKeyDown={handleKeyDown}
+                />
+                <button type="submit" className="message-submit" onClick={insertMessage}>
+                    Send
+                </button>
             </div>
-            <div class="message-box">
-                <textarea type="text" class="message-input" placeholder="Type message..."></textarea>
-                <button type="submit" class="message-submit">Send</button>
-            </div>
-
         </div>
     );
-
 };
-export default Chat;
 
-    
+export default Chat;
